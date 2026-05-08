0b932bed6cc3be68bb005b4eb77a3ddc74cb1d85
Arshidiqbaldev <arshidiqbalnet@gmail.com>
2026-05-08 09:36:43 +0500

    final v

--- script.js
+++ script.js
@@ -90,7 +90,7 @@ const mobMenu = document.getElementById("mob-menu")
 
 menuOpen.addEventListener("click", () => {
     mobMenu.classList.toggle("open-menu");
-   
+
 });
 
 menuClose.addEventListener("click", () => {
@@ -99,6 +99,28 @@ menuClose.addEventListener("click", () => {
 
 
 
+
+
+const observer = new IntersectionObserver(function (entries) {
+    entries.forEach(function (entry) {
+        if (entry.isIntersecting) {
+            entry.target.classList.add("fadeUp");
+        }
+
+        else {
+
+            entry.target.classList.remove("fadeUp");
+
+        }
+
+    });
+}, { threshold: 0.30 });
+
+const aboutElements = document.querySelectorAll(".mob-menu-item");
+
+aboutElements.forEach(el => {
+    observer.observe(el);
+})
 
 
 
