68c452f4595c22797fa10cd53d8a2bd28f52f8d7
Arshidiqbaldev <arshidiqbalnet@gmail.com>
2026-05-08 09:41:44 +0500

    final v

--- script.js
+++ script.js
@@ -114,7 +114,7 @@ const observer = new IntersectionObserver(function (entries) {
         }
 
     });
-}, { threshold: 0.30 });
+}, { threshold: 0.50 });
 
 const aboutElements = document.querySelectorAll(".mob-menu-item");
 
