4c7429faa3c391d31205310f51b7245388e3bc3c
Arshidiqbaldev <arshidiqbalnet@gmail.com>
2026-05-25 21:36:47 +0500

    final v

--- script.js
+++ script.js
@@ -1,9 +1,6 @@
 let root = document.documentElement;
 let lightMode = document.getElementById("lightBtn");
 let darkMode = document.getElementById("darkBtn");
-const myAudio1 = new Audio("assets/hover-effect-dich.mp3");
-const myAudio2 = new Audio("assets/click-1-effect-dich.mp3");
-const myAudio3 = new Audio("assets/fish-reel-in.mp3");
 
 
 
@@ -68,7 +65,7 @@ setTheme("system");
 lightMode.addEventListener("click", () => {
     applyTheme("light");
     lightMode.classList.add("active");
-    myAudio2.play();
+
     setTimeout(() => {
         darkMode.classList.remove("active");
 
@@ -79,7 +76,7 @@ lightMode.addEventListener("click", () => {
 darkMode.addEventListener("click", () => {
     applyTheme("dark");
     darkMode.classList.add("active");
-    myAudio2.play();
+
 
     setTimeout(() => {
         lightMode.classList.remove("active");
@@ -97,14 +94,14 @@ const mobMenu = document.getElementById("mob-menu")
 
 menuOpen.addEventListener("click", () => {
     mobMenu.classList.toggle("open-menu");
-    myAudio3.play();
+
 
 
 });
 
 menuClose.addEventListener("click", () => {
     mobMenu.classList.remove("open-menu");
-    myAudio2.play();
+
 })
 
 
@@ -134,26 +131,4 @@ aboutElements.forEach(el => {
 
 
 
-
-
-// sound to menu
-
-const menuItems = document.querySelectorAll(".menu-item");
-const mobmenuItems = document.querySelectorAll(".mob-menu-item");
-
-menuItems.forEach((i) => {
-
-
-
-    const handleMouseEnter = function () {
-        myAudio1.play();
-    };
-
-
-
-    i.addEventListener("mouseenter", handleMouseEnter);
-    
-
-
-})
 
